5629acfce89652792119ee5f94cafb0d0d7cf493
sakhawatalir <jam752575@gmail.com>
2025-08-31 03:18:40 +0500

    uploading website@@

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     staleTimes: {
       dynamic: 30,
     },
+  },
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
   },
 };
 
